94215953f52a885de31f6bf2d566a9fd92a6938e
Gabri3lly002 <gcesariodossantossilva@gmail.com>
2026-03-05 10:01:12 -0300

    teste

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const inputnum2 = document.getElementById('num2');
 const botaoSomar = document.getElementById('btnSomar');
 const botaoLimpar = document.getElementById('btnLimpar');
 const pResultado = document.getElementById('resultado');
+const botaoSubtrair = document.getElementById('btnSubtrair');
+const botaoMultiplicar= document.getElementById('btnMultiplicar');
+const botaoDividir = document.getElementById('btnDividir');
+//só para testar
+
 
 //Função para somar os números
 //Declaração da função acontece com: palavra reservadas function + nome da função + parênteses
@@ -58,5 +63,93 @@ function limpar() {
 };
 
 botaoLimpar.addEventListener("click",limpar);
+
+//Subtração
+function subtrair() {
+    const valor1 = inputnum1.value;
+    const valor2 = inputnum2.value;
+
+    if ( valor1 === "" || valor2 ===""){
+        pResultado.textContent = "Preencha os dois campos";
+        return;
+
+    }
+    const numero1 = Number(valor1);
+    const numero2 = Number(valor2);
+
+    if ( isNaN(numero1) ||  isNaN(numero2)) {
+        pResultado.textContent = "Digite apenas números válidos";
+        return
+
+
+    }
+    
+
+    const soma = numero1-numero2;
+    pResultado.textContent = "Resultado" +soma;
+
+
+}
+//Clicar para subtrair
+botaoSubtrair.addEventListener('click', subtrair);
+
+//Multiplicão
+function multiplicar() {
+    const valor1 = inputnum1.value;
+    const valor2 = inputnum2.value;
+
+    if ( valor1 === "" || valor2 ===""){
+        pResultado.textContent = "Preencha os dois campos";
+        return;
+
+    }
+    const numero1 = Number(valor1);
+    const numero2 = Number(valor2);
+
+    if ( isNaN(numero1) ||  isNaN(numero2)) {
+        pResultado.textContent = "Digite apenas números válidos";
+        return
+
+
+    }
+    
+
+    const soma = numero1*numero2;
+    pResultado.textContent = "Resultado:" +soma;
+
+
+}
+//Clicar para Multiplicar
+botaoMultiplicar.addEventListener('click', multiplicar);
+
+//Divisão
+function dividir() {
+    const valor1 = inputnum1.value;
+    const valor2 = inputnum2.value;
+
+    if ( valor1 === "" || valor2 ===""){
+        pResultado.textContent = "Preencha os dois campos";
+        return;
+
+    }
+    const numero1 = Number(valor1);
+    const numero2 = Number(valor2);
+
+    if ( isNaN(numero1) ||  isNaN(numero2)) {
+        pResultado.textContent = "Digite apenas números válidos";
+        return
+
+
+    }
+    
+
+    const soma = numero1/numero2;
+    pResultado.textContent = "Resultado" +soma;
+
+
+}
+//Clicar para dividir
+botaoDividir.addEventListener('click', dividir);
+
 
 
